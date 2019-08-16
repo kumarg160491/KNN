285053b1e50cb1bbc30ba25c07c2a80a0301ba11
kumarg160491 <48169382+kumarg160491@users.noreply.github.com>
2019-08-16 08:46:54 +0530

    first commit
    K-nearest neighbour

---
+++ knn_Social_Network_Ads.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 18 08:45:16 2019
+
+@author: rajkumar
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df=pd.read_csv('Social_Network_Ads.csv')
+df.head()
+
+
+#split x annd y 
+
+x=df.iloc[:,[2,3]]
+x.head()
+
+y=df.iloc[:,-1]
+y.head()
+
+#split x and y into train and test
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.25,random_state=0)
+
+
+#feature selection
+
+from sklearn.preprocessing import StandardScaler
+ssc=StandardScaler()
+
+
+x_train=ssc.fit_transform(x_train)
+x_test=ssc.fit_transform(x_test) 
+
+#KNN to training 
+
+from sklearn.neighbors import KNeighborsClassifier
+knc=KNeighborsClassifier(n_neighbors=5, metric='minkowski',p=2)
+knc.fit(x_train,y_train)
+
+# prediction
+
+y_pred=knc.predict(x_test)
+y_pred
+
+
+#confusion matrix
+from sklearn.metrics import confusion_matrix,accuracy_score
+cm=confusion_matrix(  y_test,y_pred)
+cm
+
+score=accuracy_score(  y_test,y_pred)
+score
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = x_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, knc.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('K-NN (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = x_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, knc.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('K-NN (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
